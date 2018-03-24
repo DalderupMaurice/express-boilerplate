63f06c0ee9e9bacfdf823628ade4b8828de0b382
Maurice Dalderup <Maurice_Dalderup@hotmail.com>
2018-03-24 17:54:27 +0100

    feat: added auth

--- index.route.js
+++ index.route.js
@@ -1,6 +1,8 @@
 'use strict';
 import { Router } from 'express';
+
 import userRoutes from './server/user/user.route';
+import authRoutes from './server/auth/auth.route';
 
 const router = Router(); // eslint-disable-line new-cap
 
@@ -11,5 +13,8 @@ router.get('/health-check', (req, res) => {
 
 // mount user routes at /users
 router.use('/users', userRoutes);
+
+// mount auth routes at /auth
+router.use('/auth', authRoutes);
 
 export default router;
